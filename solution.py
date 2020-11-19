a0ed0422284bfe6dabc553e20fdf7b2223a7b10b
Michał Milewski <miles98@interia.pl>
2020-11-19 21:04:46 +0100

    Wstępna rekurencja dla strips

--- solution.py
+++ solution.py
@@ -185,10 +185,6 @@ def strips(start_, goal_):
 
     while len(goals_stack) > 0:
         stack_top = goals_stack.pop()
-        # print('goals_stack: ')
-        # print(goals_stack)
-        # print('current_state: ')
-        # print(current_state)
         if isinstance(stack_top, str) and stack_top in PREDICATES.keys():
             if check_if_predicate_true_in_curent_state(stack_top, current_state):
                 continue
@@ -214,8 +210,62 @@ def strips(start_, goal_):
             operator = OPERATORS[stack_top]
             do_operator_on_current_state(operator, current_state)
             result.append(operator['name'])
-            # print('operator: ')
-            # print(operator['name'])
+    return result
+
+def strips_rek(start_, goal_, current_state_, goals_stack_, result_, first):
+    start = copy.deepcopy(start_)
+    goal = copy.deepcopy(goal_)
+
+    if first:
+        goals_stack = []
+        current_state = copy.deepcopy(start_)
+        goals_stack.append(goal)
+        result = []
+
+        for one_goal in goal:
+            if one_goal not in current_state:
+                goals_stack.append(one_goal)
+    else:
+        goals_stack = copy.deepcopy(goals_stack_)
+        current_state = copy.deepcopy(current_state_)
+        result = copy.deepcopy(result_)
+
+
+
+    while len(goals_stack) > 0:
+        stack_top = goals_stack.pop()
+        if isinstance(stack_top, str) and stack_top in PREDICATES.keys():
+            if check_if_predicate_true_in_curent_state(stack_top, current_state):
+                continue
+            operator = choose_operator_rek(goals_stack, result, stack_top, current_state)
+            if len(operator) == 0:
+                return []
+
+            for op in operator:
+                goals_stack_cp = copy.deepcopy(goals_stack)
+                goals_stack_cp.append(op['name'])
+                goals_stack_cp.append(op['pre'])
+                rt = strips_rek(copy.deepcopy(start), copy.deepcopy(goal), current_state, goals_stack_cp, result, False)
+                if len(rt) > 0:
+                    return rt
+            return []
+
+        if isinstance(stack_top, list):
+            is_every_condition_met = True
+            for pre in stack_top:
+                if not check_if_predicate_true_in_curent_state(pre, current_state):
+                    is_every_condition_met = False
+                    break
+            if not is_every_condition_met:
+                goals_stack.append(stack_top)
+                for pre in stack_top:
+                    goals_stack.append(pre)
+            continue
+
+        if isinstance(stack_top, str) and stack_top in OPERATORS.keys():
+            operator = OPERATORS[stack_top]
+            do_operator_on_current_state(operator, current_state)
+            result.append(operator['name'])
     return result
 
 
@@ -252,6 +302,36 @@ def choose_operator(goals_stack, result, stack_top, current_state):
     if len(operator) > 1:
         return operator[random.randint(0, len(operator)-1)]
 
+
+def choose_operator_rek(goals_stack, result, stack_top, current_state):
+    candidates = []
+    for operator in OPERATORS.values():
+        if stack_top in operator['add'] and operator['name'] not in result and operator['name'] not in goals_stack:
+            candidates.append(operator)
+
+    # for candidate in candidates:
+    #     for pre in candidate['pre']:
+    #         for goal in goals_stack:
+    #             if pre in goal:
+    #                 if candidate in candidates:
+    #                     candidates.remove(candidate)
+
+    # maximum = 0
+    # operator = []
+    # for op in candidates:
+    #     count = 0
+    #     for pre in op['pre']:
+    #         if check_if_predicate_true_in_curent_state(pre, current_state):
+    #             count += 1
+    #     if count > maximum:
+    #         maximum = count
+    #         operator = [op]
+    #     if count == maximum:
+    #         operator.append(op)
+
+    operator = candidates
+
+    return operator
 
 
 def do_operator_on_current_state(operator, current_state):
@@ -295,8 +375,8 @@ def compute_task(start,goal):
     s = list(start)
     g = list(goal)
     begin = time.perf_counter()
-    while len(result) == 0:
-        result = strips(s, g)
+
+    result = strips_rek(s, g, [], [], [], True)
     end = time.perf_counter()
     print(result)
     print("Time: {0:02f}s".format(end - begin))
@@ -329,5 +409,5 @@ OPERATORS = {**create_operator('PICKUP(x)', pre=['CLEAR(x)', 'ONTABLE(x)', 'ARME
     **create_operator('UNSTACK(x,y)', pre=['ON(x,y)', 'CLEAR(x)', 'ARMEMPTY'], delete=[
                     'ON(x,y)', 'ARMEMPTY'], add=['HOLDING(x)', 'CLEAR(y)'])}
 
-compute_task(start={'ON(C,B)', 'ON(B,A)', 'ONTABLE(A)', 'CLEAR(C)', 'ARMEMPTY'}, 
+compute_task(start={'ON(C,B)', 'ON(B,A)', 'ONTABLE(A)', 'CLEAR(C)', 'ARMEMPTY'},
             goal={'ON(A,B)', 'ON(B,C)', 'ONTABLE(C)', 'CLEAR(A)', 'ARMEMPTY'})
